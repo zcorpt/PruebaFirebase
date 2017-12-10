27167e201206ea55a107b1e6dbc20640067a9ed8
zcorpt <huero4@hotmail.com>
2017-12-09 23:22:12 -0600

    Vista de registro semi funcional

--- app/src/main/java/com/drapp/appinnovatio/pruebafirebase/RegistroProfesor.java
+++ app/src/main/java/com/drapp/appinnovatio/pruebafirebase/RegistroProfesor.java
@@ -1,7 +1,9 @@
 package com.drapp.appinnovatio.pruebafirebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistroProfesor extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class RegistroProfesor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_profesor);
+    }
+
+    public void RegistrarMateria (View view) {
+        Intent explicit_intent;
+        explicit_intent = new Intent(this, MainActivity.class);
+        startActivity(explicit_intent);
     }
 }
