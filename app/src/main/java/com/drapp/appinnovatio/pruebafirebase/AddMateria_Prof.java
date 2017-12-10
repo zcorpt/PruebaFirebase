b81cb28760c166d3e7c1872a96c5da436ad1ebe6
zcorpt <huero4@hotmail.com>
2017-12-09 21:16:34 -0600

    prueba para login

---
+++ app/src/main/java/com/drapp/appinnovatio/pruebafirebase/AddMateria_Prof.java
@@ -0,0 +1,13 @@
+package com.drapp.appinnovatio.pruebafirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMateria_Prof extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_materia__prof);
+    }
+}
--- app/src/main/java/com/drapp/appinnovatio/pruebafirebase/Registro.java
+++ app/src/main/java/com/drapp/appinnovatio/pruebafirebase/Registro.java
@@ -5,17 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro);
     }
-
-    public void Registar (View view) {
+    public void Validar (View view) {
         Intent explicit_intent;
-        explicit_intent = new Intent(this, Registro.class);
+        explicit_intent = new Intent(this, RegistroProfesor.class);
         startActivity(explicit_intent);
     }
+
 }
